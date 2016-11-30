d1f60485456a67a00c3e26a301f5ff0ac3fc5889
Parikshit77 <parchak@gmail.com>
2016-11-30 16:15:20 +0530

    Initial commit

--- src/serviceInstanceTests/CreateVM.java
+++ src/serviceInstanceTests/CreateVM.java
@@ -33,7 +33,7 @@ public class CreateVM {
 	    	
 	    //get VMname
 	    //vmName = args[0];
-	    vmName = "Test_VM200";
+	    vmName = "Test_VM201";
 	    
 	 
 	    ServiceInstance si = new ServiceInstance(new URL(url), user, password, true);
